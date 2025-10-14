742f558e8526b3daf582f2c7295934010795cde2
itsacsew <castillonaccel26@gmail.com>
2025-10-14 16:35:06 +0800

    1

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -16,8 +16,8 @@ const Header = () => {
                     </>
                     :
                     <>
-                        <Link className='text-sm text-blue-600 underline' to={'/login'}>Login</Link>
-                        <Link className='text-sm text-blue-600 underline' to={'/register'}>Register New Account</Link>
+                        <Link className='text-sm text-blue-600 underline' to={'/login'}></Link>
+                        <Link className='text-sm text-blue-600 underline' to={'/register'}></Link>
                     </>
             }
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+import {getAuth} from "firebase/auth"
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "your_apiKey",
-    authDomain: "your_authDomain",
-    projectId: "your_projectId",
-    storageBucket: "your_storageBucket",
-    messagingSenderId: "your_messagingSenderId",
-    appId: "your_appId"
-  };
+  apiKey: "AIzaSyDFFfVS8xUBiMwK5mt_-c10qS072VZLoS4",
+  authDomain: "login-188a5.firebaseapp.com",
+  projectId: "login-188a5",
+  storageBucket: "login-188a5.firebasestorage.app",
+  messagingSenderId: "760693045901",
+  appId: "1:760693045901:web:e7239104d9e45a052700c4"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 
-
-
-export { app, auth };
+export {app, auth};
